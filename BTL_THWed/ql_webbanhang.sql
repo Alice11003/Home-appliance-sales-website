c8445e2537336a8ca4b06323eb4dfe9bd1815721
Vương Thúy Quỳnh <vuongquynhhda@gmail.com>
2025-07-10 16:37:22 +0700

    ql_webbanhang.sql

--- BTL_THWed/ql_webbanhang.sql
+++ BTL_THWed/ql_webbanhang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2024 at 08:45 AM
+-- Generation Time: Oct 15, 2024 at 08:45 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
